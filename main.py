f79799d5fb5e195347e4743bbcd392466d33cdb6
arseny atnashev <112005553+aboba485@users.noreply.github.com>
2024-12-01 10:13:11 +0200

    update 2.0!

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ CELLS_COLOR = (31, 31, 31)
 NUMBERS_COLOR = (35, 35, 35)
 START_X = 50
 START_Y = 50
-counter=0
+counter = 0
 SIZE_OF_THE_CUBE = (SIZE_OF_THE_DISPLAY - START_X - START_X) // SIZE_OF_THE_FIELD
 changed_field = logic.get_field2(SIZE_OF_THE_FIELD)
 start_ticks = pygame.time.get_ticks()
 pygame.display.set_caption('MineSweeper')
-print(changed_field)
+
 
 running = True
 while running:
@@ -37,25 +37,19 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
         if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = pygame.mouse.get_pos()
-                if START_X < x < SIZE_OF_THE_DISPLAY - START_X and START_Y < y < SIZE_OF_THE_DISPLAY - START_Y:
-                    if pygame.mouse.get_pressed()[0]:
-                        changed_field = logic.define_coordinate(START_X, START_Y, x, y, SIZE_OF_THE_DISPLAY,SIZE_OF_THE_CUBE, changed_field, FIELD, SIZE_OF_THE_FIELD )
-                    elif pygame.mouse.get_pressed()[2]:
-                        counter2=logic.CountFlags(changed_field, SIZE_OF_THE_FIELD)
-                        if counter2 < NUMBER_OF_BOMBS:
-                                changed_field = logic.place_flag(START_X, START_Y, x, y, SIZE_OF_THE_DISPLAY, SIZE_OF_THE_CUBE, changed_field)
+            x, y = pygame.mouse.get_pos()
+            if START_X < x < SIZE_OF_THE_DISPLAY - START_X and START_Y < y < SIZE_OF_THE_DISPLAY - START_Y:
+                if pygame.mouse.get_pressed()[0]:
+                    changed_field = logic.define_coordinate(START_X, START_Y, x, y, SIZE_OF_THE_DISPLAY, SIZE_OF_THE_CUBE, changed_field, FIELD, SIZE_OF_THE_FIELD)
+                elif pygame.mouse.get_pressed()[2]:
+                    counter2 = logic.count_flags(changed_field, SIZE_OF_THE_FIELD)
+                    if counter2 < NUMBER_OF_BOMBS:
+                        changed_field = logic.place_flag(START_X, START_Y, x, y, SIZE_OF_THE_DISPLAY, SIZE_OF_THE_CUBE, changed_field)
         counter = 0
-        counter = logic.CountCorrectFlags(FIELD, changed_field, SIZE_OF_THE_FIELD)
+        counter = logic.count_correct_flags(FIELD, changed_field, SIZE_OF_THE_FIELD)
         if counter == NUMBER_OF_BOMBS:
             running = False
-
-
-
-
-
 
     pygame.display.flip()
 
